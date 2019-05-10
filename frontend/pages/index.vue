b0326d0781e6c4ee28528c82c8999cec0ce6e624
shunpei <koikeshunpei@koikeshunpeinoMacBook-Pro.local>
2019-05-10 23:48:20 +0900

    add frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <a-scene>
+          <a-box color="#658568" width="1" height="1" depth="1"></a-box>
+        </a-scene>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import 'aframe'
+
+export default {
+  components: {}
+}
+</script>
