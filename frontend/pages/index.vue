e2f0e95201a8bbc5ce30fffe5f71a3ca9f8dcc75
shunpei <shunpei42ba@gmail.com>
2019-05-12 00:00:57 +0900

    add sample image

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,30 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <a-scene>
-          <a-box color="#658568" width="1" height="1" depth="1"></a-box>
-        </a-scene>
-      </div>
-    </v-flex>
-  </v-layout>
+  <a-scene>
+    <a-assets>
+      <img id="floor" :src="floorImg">
+    </a-assets>
+    <a-sky color="#6e6eff"></a-sky>
+    <a-plane
+      src="#floor"
+      rotation="-90 0 0"
+      scale="10000 10000 1"
+      repeat="1000 1000"
+      shadow="receive: true"
+    ></a-plane>
+    <a-box color="#658568" width="2" height="2" depth="1"></a-box>
+  </a-scene>
 </template>
 
 <script>
 import 'aframe'
+import FloorImg from '~/assets/img/floor-1024.jpg'
 
 export default {
-  components: {}
+  components: {},
+  data() {
+    return {
+      floorImg: FloorImg
+    }
+  }
 }
 </script>
