2bdf4b61f311f1962370718593e22613e8605e42
shunpei <shunpei42ba@gmail.com>
2019-05-12 17:16:20 +0900

    add animation

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <a-scene>
+    <!-- Assets -->
     <a-assets>
       <img id="floor" :src="floorImg">
+      <!-- <img id="firefox" src="firefoxImg"> -->
     </a-assets>
-    <a-sky color="#6e6eff"></a-sky>
+    <!-- Sky -->
+    <a-sky color="#212121"></a-sky>
+    <!-- Ground -->
     <a-plane
       src="#floor"
       rotation="-90 0 0"
@@ -11,19 +15,38 @@
       repeat="1000 1000"
       shadow="receive: true"
     ></a-plane>
-    <a-box color="#658568" width="2" height="2" depth="1"></a-box>
+    <!-- Camera -->
+
+    <!-- Yellow Box -->
+    <a-box
+      color="#FFEB3B"
+      scale="0.5 0.5 0.5"
+      position="-1 1 0"
+      animation="property: rotation; to: 0 360 0; loop: true; dur: 5000; easing: linear"
+    ></a-box>
+
+    <!-- Orange Box -->
+    <a-box
+      id="orage-box"
+      color="#FF5722"
+      scale="0.5 0.5 0.5"
+      position="1 1 0"
+      animation="property: rotation; to: 0 360 0; loop: true; dur: 5000; easing:linear"
+    ></a-box>
   </a-scene>
 </template>
 
 <script>
 import 'aframe'
 import FloorImg from '~/assets/img/floor-1024.jpg'
+// import FireFoxImg from '~/assets/img/firefox.png'
 
 export default {
   components: {},
   data() {
     return {
       floorImg: FloorImg
+      // fireFoxImg: FireFoxImg
     }
   }
 }
